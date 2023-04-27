f52fda928c8659c7c45fb2c453a8e06cf97ea2b7
suganthiganapathy <suganthi1702.kodehode@gmail.com>
2023-04-27 14:57:54 +0200

    Added all files

---
+++ index.js
@@ -0,0 +1,32 @@
+let homeCnt=document.getElementById("homebox");
+let guestCnt=document.getElementById("guestbox");
+let num=0;
+let number=0;
+
+
+function addOne(){
+   num=num+1;
+   homeCnt.textContent=num;
+}
+
+function addtwo(){
+    num=num+2;
+    homeCnt.textContent=num;
+ }
+
+ function addthree(){
+    num=num+3;
+    homeCnt.textContent=num;
+ }
+ function addfour(){
+    number=number+1;
+    guestCnt.textContent=number;
+ }
+ function addfive(){
+    number=number+2;
+    guestCnt.textContent=number;
+ }
+ function addsix(){
+    number=number+3;
+    guestCnt.textContent=number;
+ }
